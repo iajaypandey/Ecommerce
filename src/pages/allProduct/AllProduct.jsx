b8fbcc6eed6893d0dc7cd85926353614d4f50746
Ajay Pandey <ajaypandeyy1123@gmail.com>
2025-08-08 03:00:34 +0530

    update in pages/allproduct/AllProduct

--- src/pages/allProduct/AllProduct.jsx
+++ src/pages/allProduct/AllProduct.jsx
@@ -7,81 +7,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { addToCart, deleteFromCart } from "../../redux/cartSlice";
 import toast from "react-hot-toast";
 
-// // productData 
-// const productData = [
-//     {
-//         id: 1,
-//         image: 'https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg',
-//         title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-//         desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-//         price: 150,
-//         trendingProductName: 'Featured',
-//         quantity: 1,
-//     },
-//     {
-//         id: 2,
-//         image: 'https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg',
-//         title: 'Kaushalam kalash Copper Pot',
-//         desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-//         price: 120,
-//         trendingProductName: 'Featured',
-//         quantity: 1,
-//     },
-//     {
-//         id: 3,
-//         image: 'https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg',
-//         title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-//         desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-//         price: 130,
-//         trendingProductName: 'Featured',
-//         quantity: 1,
-//     },
-//     {
-//         id: 4,
-//         image: 'https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg',
-//         title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-//         desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-//         price: 120,
-//         trendingProductName: 'Featured',
-//         quantity: 1,
-//     },
-//     {
-//         id: 1,
-//         image: 'https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg',
-//         title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-//         desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-//         price: 150,
-//         trendingProductName: 'Featured',
-//         quantity: 1,
-//     },
-//     {
-//         id: 2,
-//         image: 'https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg',
-//         title: 'Kaushalam kalash Copper Pot',
-//         desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-//         price: 120,
-//         trendingProductName: 'Featured',
-//         quantity: 1,
-//     },
-//     {
-//         id: 3,
-//         image: 'https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg',
-//         title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-//         desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-//         price: 130,
-//         trendingProductName: 'Featured',
-//         quantity: 1,
-//     },
-//     {
-//         id: 4,
-//         image: 'https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg',
-//         title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-//         desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-//         price: 120,
-//         trendingProductName: 'Featured',
-//         quantity: 1,
-//     }
-// ]
 
 const AllProduct = () => {
     const context = useContext(MyContext);
@@ -102,7 +27,11 @@ const AllProduct = () => {
     }
 
     const deleteCart = (item) => {
-        dispatch(deleteFromCart(item));
+        const sanitizedItem = {
+            ...item,
+            time : item.time?.toDate?.().toISOString?.() || null,
+        };
+        dispatch(deleteFromCart(sanitizedItem));
         toast.success("Delete cart")
     }
 
